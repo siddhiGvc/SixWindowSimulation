3a0f4c2b41605e1662b89df199c35223f2c8f2d3
siddhi@gvc.in <siddhidhamnaskar6@gmail.com>
2024-09-09 11:53:36 +0530

    minor changes

--- src/SixWindows/user-table-row.jsx
+++ src/SixWindows/user-table-row.jsx
@@ -211,7 +211,7 @@ export default function UserTableRow({
       controls 
       autoPlay  
       onEnded={handleVideoEnd}
-      muted={isMuted}
+      muted
     >
       <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
       <track src="path-to-your-captions.vtt" kind="captions" srcLang="en" label="English" />
